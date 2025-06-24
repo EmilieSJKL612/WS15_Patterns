4a90e3f29894868ddd83d4a0ce40181395e46181
EmilieSJKL612 <151094902+EmilieSJKL612@users.noreply.github.com>
2025-06-24 02:06:23 -0600

    Add files via upload

--- timing.js
+++ timing.js
@@ -19,7 +19,18 @@ export function isSongInPlaylist(playlist, song) {
  * @returns `undefined` if `numbers` is empty
  */
 export function isAllEven(numbers) {
-  // TODO
+  // TODO - done
+  if (numbers.length === 0){
+    return undefined;
+  }
+
+  for (const num of numbers){
+    if (num % 2 !== 0){
+      return false;
+    }
+  }
+return true;
+
 }
 
 /**
@@ -32,5 +43,18 @@ export function isAllEven(numbers) {
  * @returns {boolean} whether there is enough food in the backpack to feed everyone
  */
 export function haveEnoughFood(backpack, people) {
-  // TODO
+  // TODO - done
+  if (people === 0){
+    return true;
+  }
+
+  let foodCount = 0;
+  for (const item of backpack){
+    if (item.category === "food"){
+      foodCount++;
+    }
+  }
+
+  return foodCount >= people;
+
 }
